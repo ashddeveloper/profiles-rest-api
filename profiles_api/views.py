eda71e691c394fcf04b75c5939b7a7ba0e5ef8f1
ash_developer <ashthedeveloper@gmail.com>
2019-12-16 16:02:07 +0530

    added viewset to profiles_api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import viewsets
 
 from profiles_api import serializers
 
@@ -40,10 +41,60 @@ class HellowApiView(APIView):
         """handle updating an object"""
         return Response({'method':'PUT'})
 
-    def patch(self, request pk=None):
+    def patch(self, request, pk=None):
         """handle a partial update of an object"""
         return Response({'method':'PATCH'})
 
-    def delete(self, request pk=None):
+    def delete(self, request, pk=None):
         """delete an object"""
         return Response({'method':'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+        """Return a hellow message"""
+
+        a_viewset = [
+        'Uses actions (list,create,retrieve,update,partial_update)',
+        'Automatically map to URLs using routers',
+        ' provides more functionality with lesser codes',
+        ]
+
+        return Response({'message':'Hellooo' , 'a_viewset':a_viewset})
+
+    def create(Self, request):
+        """create a new hello message"""
+        serializer = self.serializer_class(data =request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name} !'
+            return Response({'message' :message})
+        else:
+            return Response(
+            serializer.errors,
+            status = status.HTTP_400_BAD_REQUEST
+            )
+
+
+    def retrieve(self, request, pk=None):
+        """handle getting an object by its ID"""
+
+        return Response({'http_method':'GET'})
+
+    def update(self, request, pk=None):
+        """handle updating an object by its ID"""
+        return Response({'http_method':'PUT'})
+
+
+    def partial_update(self, request, pk=None):
+        """handle partial_updating an object by its ID"""
+        return Response({'http_method':'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """handle removing an object by its ID"""
+        return Response({'http_method':'DELETE'})
